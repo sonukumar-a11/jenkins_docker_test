ce64b0147ae4e4026d10cb6c3acc19642f642b50
sonukumar-a11 <sharmasonu04359491@gmail.com>
2021-10-25 17:14:05 +0530

    Upsert the All feature with considration all testase

--- test.py
+++ test.py
@@ -6,7 +6,10 @@ class InequalityTest(unittest.TestCase):
         self.assertEqual(anagram_check.check_anagaram("listen" , "silent") , "listen is anagrams with silent")
 
     def test_not_equal(self):
-        self.assertEqual(anagram_check.check_anagaram("Sonu" , "Monu") , "Sonu aren't anagrams with Monu")
+        self.assertNotEqual(anagram_check.check_anagaram("Sonu" , "Mnuo") , "Sonu aren't anagrams with Monu")
+    
+    def test_not_found(self):
+        self.assertEqual(anagram_check.check_anagaram("Sonu" , "Mnuo") , "Sonu aren't anagrams with Mnuo")
 
 if __name__ == '__main__':
     unittest.main()
